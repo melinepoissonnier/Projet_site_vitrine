b97d4e072fecd613aefa6c50c43c9b9c1f1b0d46
Huor97 <huor97ka@gmail.com>
2021-07-29 19:25:45 +0200

    V07 finaliser html css js pg actuel

--- actuels.js
+++ actuels.js
@@ -1,43 +1,48 @@
+/**
+ * crée un événement qui pérmer de changer les photos si mouse est sur un image pour chaques cards.
+ * crée un événement qui pérmer de changer les photos si mouse enlever sur une image pour chaques cards.
+ */
+// card 1: si mouse est sur ce image
 img1.addEventListener("mouseover", (e) => {
   img1.setAttribute("src", "./images/orchidee_dior.jpg");
 });
-
+// card 1: si mouse n'est pas sur ce image
 img1.addEventListener("mouseout", (e) => {
   img1.setAttribute("src", "./images/orchidee_dior1.jpg");
 });
-
+// card 2: si mouse est sur ce image
 img2.addEventListener("mouseover", (e) => {
   img2.setAttribute("src", "./images/slow_flower2.jpg");
 });
-
+// card 2: si mouse n'est pas sur ce image
 img2.addEventListener("mouseout", (e) => {
   img2.setAttribute("src", "./images/slow_flower.jpg");
 });
-
+// card 3: si mouse est sur ce image
 img3.addEventListener("mouseover", (e) => {
   img3.setAttribute("src", "./images/hortensia_bretagne2.jpg");
 });
-
+// card 3: si mouse n'est pas sur ce image
 img3.addEventListener("mouseout", (e) => {
   img3.setAttribute("src", "./images/hortensia_bretagne.jpg");
 });
-
+// card 4: si mouse est sur ce image
 img4.addEventListener("mouseover", (e) => {
   img4.setAttribute("src", "./images/art_floral2.jpg");
 });
-
+// card 4: si mouse n'est pas sur ce image
 img4.addEventListener("mouseout", (e) => {
   img4.setAttribute("src", "./images/art_floral.jpg");
 });
 
-//Get the button
+//récuperer button scroller pour aller début
 let mybutton = document.getElementById("btn-back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+//Lorsque l'utilisateur fait défiler 20px du haut du document vers le bas, affichez le bouton
 window.onscroll = function () {
   scrollFunction();
 };
-
+// program: condition si utilisateur fait défiler 20px du haut du document vers le bas, affichez le bouton
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -45,7 +50,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
+// Lorsque l'utilisateur clique sur le bouton, faites défiler vers le haut du document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
