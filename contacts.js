d2e6ed5ff64c97b2d507889c87d328855c7d8051
Alexis Doroszkiewicz <75637000+AlexisDoroszkiewicz@users.noreply.github.com>
2021-07-29 13:43:48 +0200

    le push du dégout

--- contacts.js
+++ contacts.js
@@ -1,14 +1,17 @@
-let burger = document.getElementById("burger")
-let clicked = false
+// let burger = document.getElementById("burger")
+// let clicked = false
 
-burger.addEventListener("click", function(e){
-    if (clicked === false) {
-        document.getElementById("contactform").style.transform = "translate(0, -205px)";
-        clicked = true;
-    }
-   else if (clicked === true) {
-    document.getElementById("contactform").style.transform = "translate(0)";
-    clicked = false;
-   }
-})
+// burger.addEventListener("click", function(e){
+//     if (clicked === false) {
+//         document.getElementById("contactform").style.transform = "translate(0, -205px)";
+//         clicked = true;
+        
+//     }
+//    else if (clicked === true) {
+//     document.getElementById("contactform").style.transform = "translate(0)";
+//     clicked = false;
+    
+//    }
+  
+// })
 
